52bc6b71820c9e33906812e685f29fcf38989a32
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-03 09:18:08 +0300

    Настройка маршрута

--- ASP.NET_EducationPlatform/Program.cs
+++ ASP.NET_EducationPlatform/Program.cs
@@ -1,9 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 var services = builder.Services; // сервисы
 services.AddControllersWithViews();
 
+var app = builder.Build();
+
+//__________________________________//
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // вывод ошибок
+}
+
+app.UseRouting(); // система маршрутизации
+app.UseStaticFiles(); // статические файлы wwwroot
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapControllerRoute( 
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 app.Run();
