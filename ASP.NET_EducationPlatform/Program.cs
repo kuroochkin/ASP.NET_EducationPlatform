36790a81ca6a0b55df2b7ff8ce613380d8eb09d8
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-03 08:57:30 +0300

    Добавление сервиса контроллеров и представлений

--- ASP.NET_EducationPlatform/Program.cs
+++ ASP.NET_EducationPlatform/Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+var services = builder.Services; // сервисы
+services.AddControllersWithViews();
+
+
 app.MapGet("/", () => "Hello World!");
 app.Run();
