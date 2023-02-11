dfd245ac82637ae159212514a96e9723c672706f
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-12 01:15:36 +0300

    Подключение сервиса уроков

---
+++ ASP.NET_EducationPlatform/Controllers/LessonController.cs
@@ -0,0 +1,31 @@
+﻿using ASP.NET_EducationPlatform.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_EducationPlatform.Controllers
+{
+    public class LessonController : Controller
+    {
+        private readonly ILessonData _lessonsData;
+
+        public LessonController(ILessonData lessonData)
+        {
+            _lessonsData = lessonData;
+        }
+
+        public IActionResult Index()
+        {
+            var lessons = _lessonsData.GetAllLessons();
+
+            return View(lessons);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var lesson = _lessonsData.GetById(id);
+            if (lesson is null)
+                return NotFound();
+
+            return View(lesson);
+        }
+    }
+}
--- ASP.NET_EducationPlatform/Data/TestData.cs
+++ ASP.NET_EducationPlatform/Data/TestData.cs
@@ -5,20 +5,20 @@ namespace ASP.NET_EducationPlatform.Data
 {
     public static class TestData
     {
-        public static List<Teacher> teachers { get; } = new()
+        public static List<Teacher> teachers { get; set; } = new()
         {
             new Teacher {Id = 1, LastName = "Келин", FirstName = "Кирилл", Patronymic = "Вячеславович"},
             new Teacher {Id = 2, LastName = "Курочкин", FirstName = "Владислав", Patronymic = "Романович"},
         };
 
 
-        public static List<Student> students { get; } = new()
+        public static List<Student> students { get; set; } = new()
         {
             new Student {Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", YearStudy = 9},
             new Student {Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", YearStudy = 11},
         };
 
-        public static List<Subject> subjects { get; } = new()
+        public static List<Subject> subjects { get; set; } = new()
         {
             new Subject{Id = 1, Name = "Физика", IsInvolved = false},
             new Subject{Id = 2, Name = "Математика", IsInvolved = false},
@@ -27,12 +27,12 @@ namespace ASP.NET_EducationPlatform.Data
             new Subject{Id = 2, Name = "География", IsInvolved = false},
         };
 
-        public static List<Lesson> lessons { get; } = new()
+        public static List<Lesson> lessons { get; set; } = new()
         {
             new Lesson
             {
                 Id = 1,
-                DateTime = new DateTime(5,6,2023),
+                DateTime = new DateTime(2023,6,6),
                 Subject = new Subject { Id = 1, Name = "Физика", IsInvolved = false },
                 Direction = "ОГЭ",
                 Teacher = new Teacher {Id = 1, LastName = "Келин", FirstName = "Кирилл", Patronymic = "Вячеславович"},
