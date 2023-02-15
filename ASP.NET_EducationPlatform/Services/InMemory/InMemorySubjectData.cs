a1aa53a8803234fd9357636dc9bb475dd3c02207
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-15 20:51:49 +0300

    Реализация сервиса предметов

---
+++ ASP.NET_EducationPlatform/Services/InMemory/InMemorySubjectData.cs
@@ -0,0 +1,22 @@
+﻿using ASP.NET_EducationPlatform.Data;
+using ASP.NET_EducationPlatform.Services.Interfaces;
+using EducationPlatfotm.Domain;
+using EducationPlatfotm.Domain.Users;
+
+namespace ASP.NET_EducationPlatform.Services.InMemory
+{
+    public class InMemorySubjectData : ISubjectData
+    {
+        private ICollection<Subject> _subjects;
+
+        public InMemorySubjectData()
+        {
+            _subjects = TestData.subjects;
+        }
+
+        public Subject? GetById(int id)
+        {
+            return _subjects.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
