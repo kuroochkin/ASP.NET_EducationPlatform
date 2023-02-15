e00da6b6b3f76835d464ec58f7406393f4b52f58
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-02-15 20:51:32 +0300

    Интерфейс сервиса предметов

---
+++ ASP.NET_EducationPlatform/Services/Interfaces/ISubjectData.cs
@@ -0,0 +1,9 @@
+﻿using EducationPlatfotm.Domain;
+
+namespace ASP.NET_EducationPlatform.Services.Interfaces
+{
+    public interface ISubjectData
+    {
+        public Subject? GetById(int id);
+    }
+}
